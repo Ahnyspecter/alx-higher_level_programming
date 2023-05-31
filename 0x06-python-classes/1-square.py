aca6fe2aba3d44e9191a8b4ddbd2c1cc3db3af2b
Ahnyspecter <mspecter15@gmail.com>
2023-05-30 17:25:39 -0700

    all task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Definds the class Square."""
+
+
+class Square:
+    """Represented the square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square."""
+        self.__size = size
